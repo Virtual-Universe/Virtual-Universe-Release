b1398d6a70ecd011fb4e5b03e6744d7b9c91ed2d
EmperorStarfinder <emperor@secondgalaxy.com>
2013-09-18 16:18:28 -0400

    Initial commit of the CoreApplicationplugins code.  Note I may need to make fixes to this for linux and windows.  We will see though.
    Signed-off-by: EmperorStarfinder <emperor@secondgalaxy.com>

---
+++ VirtualUniverse/CoreApplicationPlugins/ServiceLoader/ServiceLoader.cs
@@ -0,0 +1,99 @@
+﻿/*
+ * Software license for this code will be added after the code is completed.
+ * This code is in the early stages of development so should not be utilized 
+ * at this time.
+ * 
+ * Thanks
+ * Virtual Universe Development Team
+ */
+
+using VirtualUniverse.Framework.ModuleLoader;
+using VirtualUniverse.Framework.Modules;
+using VirtualUniverse.Framework.Services;
+using Nini.Config;
+using System.Collections.Generic;
+
+namespace VirtualUniverse.CoreApplicationPlugins.ServicesLoader
+{
+    public class ServicesLoader : IApplicationPlugin
+    {
+        private ISimulationBase m_openSim;
+
+        #region IApplicationPlugin Members
+
+        public void PreStartup(ISimulationBase simBase)
+        {
+        }
+
+        public void Initialize(ISimulationBase openSim)
+        {
+            m_openSim = openSim;
+        }
+
+        public void ReloadConfiguration(IConfigSource config)
+        {
+        }
+
+        public void PostInitialise()
+        {
+        }
+
+        public void Start()
+        {
+            IConfig handlerConfig = m_openSim.ConfigSource.Configs["ApplicationPlugins"];
+            if (handlerConfig.GetString("ServicesLoader", "") != Name)
+                return;
+
+            List<IService> serviceConnectors = VirtualUniverseModuleLoader.PickupModules<IService>();
+            foreach (IService connector in serviceConnectors)
+            {
+                try
+                {
+                    connector.Initialize(m_openSim.ConfigSource, m_openSim.ApplicationRegistry);
+                }
+                catch
+                {
+                }
+            }
+            foreach (IService connector in serviceConnectors)
+            {
+                try
+                {
+                    connector.Start(m_openSim.ConfigSource, m_openSim.ApplicationRegistry);
+                }
+                catch
+                {
+                }
+            }
+            foreach (IService connector in serviceConnectors)
+            {
+                try
+                {
+                    connector.FinishedStartup();
+                }
+                catch
+                {
+                }
+            }
+        }
+
+        public void PostStart()
+        {
+        }
+
+        public void Close()
+        {
+        }
+
+        public string Name
+        {
+            get { return "ServicesLoader"; }
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+        }
+    }
+}
