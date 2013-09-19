7bb93d27ddf9fd48fc8000e5607ca77fb8ec058f
EmperorStarfinder <emperor@secondgalaxy.com>
2013-09-18 20:26:35 -0400

    Commit the webserver directory.  This is not the built in web pages.
    Signed-off-by: EmperorStarfinder <emperor@secondgalaxy.com>

---
+++ VirtualUniverse/WebServer/ServerBase.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Software license for this code will be added after the code is completed.
+ * This code is in the early stages of development so should not be utilized 
+ * at this time.
+ * 
+ * Thanks
+ * Virtual Universe Development Team
+ */
+
+using VirtualUniverse.Framework;
+using VirtualUniverse.Framework.ConsoleFramework;
+using VirtualUniverse.Framework.Modules;
+using VirtualUniverse.Simulation.Base;
+using System;
+
+namespace VirtualUniverse.Server
+{
+    public class VirtualUniverseBase : SimulationBase
+    {
+        /// <summary>
+        ///     Performs initialisation of the scene, such as loading configuration from disk.
+        /// </summary>
+        public override void Startup()
+        {
+            base.Startup();
+
+            //Fix the default prompt
+            if (MainConsole.Instance != null)
+            {
+                MainConsole.Instance.DefaultPrompt = "VirtualUniverse.WebServer ";
+                MainConsole.Instance.Info("[VIRTUALUNIVERSESTARTUP]: Startup completed in " +
+                                          (DateTime.Now - this.StartupTime).TotalSeconds);
+            }
+        }
+
+        public override ISimulationBase Copy()
+        {
+            return new VirtualUniverseBase();
+        }
+    }
+}
