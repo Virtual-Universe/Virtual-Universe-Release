80da4beb6e4cf2caf2e875d09e1f0baf8f6bf188
EmperorStarfinder <emperor@secondgalaxy.com>
2013-09-20 10:20:06 -0400

    Commit the Server and Servers Directory. Note we may just combine these two directories at some point.
    Signed-off-by: EmperorStarfinder <emperor@secondgalaxy.com>

---
+++ VirtualUniverse/Server/Application.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * Software license for this code will be added after the code is completed.
+ * This code is in the early stages of development so should not be utilized 
+ * at this time.
+ * 
+ * Thanks
+ * Virtual Universe Development Team
+ */
+
+using VirtualUniverse.Simulation.Base;
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+
+namespace VirtualUniverse.Server
+{
+    /// <summary>
+    ///     Starting class for the VirtualUniverse Server
+    /// </summary>
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            BaseApplication.BaseMain(args, "VirtualUniverse.Server.ini", new VirtualUniverseBase());
+        }
+    }
+}
---
+++ VirtualUniverse/Servers/AvatarServer/Application.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * Software license for this code will be added after the code is completed.
+ * This code is in the early stages of development so should not be utilized 
+ * at this time.
+ * 
+ * Thanks
+ * Virtual Universe Development Team
+ */
+
+using System;
+using System.Collections.Generic;
+using VirtualUniverse.Framework;
+using VirtualUniverse.Framework.Services;
+using VirtualUniverse.Simulation.Base;
+using VirtualUniverse.Framework.Modules;
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0")]
+
+namespace VirtualUniverse.Servers.AvatarServer
+{
+    /// <summary>
+    ///     Starting class for the VirtualUniverse Server
+    /// </summary>
+    public class Application
+    {
+        public static void Main(string[] args)
+        {
+            BaseApplication.BaseMain(args, "VirtualUniverse.AvatarServer.ini",
+                                     new MinimalSimulationBase("VirtualUniverse.AvatarServer ",
+                                                               new List<Type>
+                                                                   {
+                                                                       typeof (IAvatarData),
+                                                                       typeof (IInventoryData),
+                                                                       typeof (IUserAccountData),
+                                                                       typeof (IAssetDataPlugin)
+                                                                   },
+                                                               new List<Type>
+                                                                   {
+                                                                       typeof (IAvatarService),
+                                                                       typeof (IInventoryService),
+                                                                       typeof (IUserAccountService),
+                                                                       typeof (IAssetService),
+                                                                       typeof (ISyncMessagePosterService),
+                                                                       typeof (ISyncMessageRecievedService),
+                                                                       typeof (IExternalCapsHandler),
+                                                                       typeof (IConfigurationService),
+                                                                       typeof (IGridServerInfoService),
+                                                                       typeof (IAgentAppearanceService),
+                                                                       typeof (IJ2KDecoder)
+                                                                   }));
+        }
+    }
+}
