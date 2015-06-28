c64748042c6b61f36dc0db0e487bbc2a91a4c83a
NoahStarfinder <noah@secondgalaxy.com>
2015-06-28 15:32:00 -0400

    Fix accidental mistake with AssemblyInfo
    Signed-off-by: NoahStarfinder <noah@secondgalaxy.com>

--- Universe/Services/DataService/Properties/AssemblyInfo.cs
+++ Universe/Services/DataService/Properties/AssemblyInfo.cs
@@ -40,7 +40,7 @@ using System.Resources;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Virtual Universe Development Team")]
-[assembly: AssemblyProduct("Virtual-Universe")]
+[assembly: AssemblyProduct("Universe.Services.DataService")]
 [assembly: AssemblyCopyright("Copyright © 2015-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
