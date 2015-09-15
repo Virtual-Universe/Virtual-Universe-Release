a7e5d959f9aeea603b8bcfa7546382af96a5bb84
EmperorStarfinder <emperor@secondgalaxy.com>
2015-09-15 12:41:15 -0400

    Fix an error message that was not being displayed correctly.
    Signed-off-by: EmperorStarfinder <emperor@secondgalaxy.com>

--- Universe/Services/SQLServices/UserAccountService/UserAccountService.cs
+++ Universe/Services/SQLServices/UserAccountService/UserAccountService.cs
@@ -1478,7 +1478,7 @@ namespace Universe.Services.SQLServices.UserAccountService
                     string check = CreateUser(UserUUID, UUID.Zero, FirstName + " " + LastName, Util.Md5Hash(Password), Email);
                     if (check != "")
                     {
-                        MainConsole.Instance.ErrorFormat("Couldn't create the user'{0} {1}'. Reason: {2}",
+                        MainConsole.Instance.ErrorFormat("Couldn't create the user '{0} {1}'. Reason: {2}",
                             FirstName, LastName, check);
                         continue;
                     }
