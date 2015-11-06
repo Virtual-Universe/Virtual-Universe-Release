7d1bd41c58b4244dcf1dd79c111c6c34680c3bf3
EmperorStarfinder <emperor@secondgalaxy.com>
2015-11-05 19:49:55 -0500

    Cleaning up the DatabaseInterfaces
    Signed-off-by: EmperorStarfinder <emperor@secondgalaxy.com>

--- Universe/Framework/DatabaseInterfaces/IEmailConnector.cs
+++ Universe/Framework/DatabaseInterfaces/IEmailConnector.cs
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) Contributors, http://virtual-planets.org/, http://whitecore-sim.org/, http://aurora-sim.org
+﻿/*
+ * Copyright (c) Contributors, http://virtual-planets.org/, http://whitecore-sim.org/, http://aurora-sim.org, http://opensimulator.org/
  * See CONTRIBUTORS.TXT for a full list of copyright holders.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -9,7 +9,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Virtual Universe Project nor the
+ *     * Neither the name of the Virtual-Universe Project nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
@@ -26,10 +26,10 @@
  */
 
 using System.Collections.Generic;
-using Universe.Framework.Modules;
-using Universe.Framework.Services;
 using OpenMetaverse;
 using OpenMetaverse.StructuredData;
+using Universe.Framework.Modules;
+using Universe.Framework.Services;
 
 namespace Universe.Framework.DatabaseInterfaces
 {
@@ -65,7 +65,7 @@ namespace Universe.Framework.DatabaseInterfaces
         }
     }
 
-    public interface IEmailConnector : IUniverseDataPlugin
+    public interface IEmailConnector : IWhiteCoreDataPlugin
     {
         /// <summary>
         ///     Adds an email to the database for the prim to get later
