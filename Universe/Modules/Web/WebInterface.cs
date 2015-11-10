395cca701f6b1bf8c8ce85388bc42c0394481d90
EmperorStarfinder <emperor@secondgalaxy.com>
2015-11-10 10:44:02 -0500

    reverse two of the changes accidentally made
    Signed-off-by: EmperorStarfinder <emperor@secondgalaxy.com>

--- Universe/Modules/Web/WebInterface.cs
+++ Universe/Modules/Web/WebInterface.cs
@@ -1125,9 +1125,9 @@ namespace Universe.Modules.Web
 
     public class GridSettings : IDataTransferable
     {
-        public string Gridname = "Universe Grid";
+        public string Gridname = "Virtual Universe Grid";
         public string Gridnick = "Universe";
-        public string WelcomeMessage = "Welcome to Universe, <USERNAME>!";
+        public string WelcomeMessage = "Welcome to Virtual Universe, <USERNAME>!";
         public string SystemEstateOwnerName = Constants.GovernorName;
         public string SystemEstateName = Constants.SystemEstateName;
 
