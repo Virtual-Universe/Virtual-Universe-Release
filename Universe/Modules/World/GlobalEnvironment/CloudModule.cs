54c938ffb70b258591ec2160e133b5604c1926ec
NoahStarfinder <noah@secondgalaxy.com>
2015-12-01 02:18:37 -0500

    Correct namespace in the GlobalEnvironment module
    - Correct the namespace
    - This does not impact functionality
    
    Signed-off-by: NoahStarfinder <noah@secondgalaxy.com>

--- Universe/Modules/World/GlobalEnvironment/CloudModule.cs
+++ Universe/Modules/World/GlobalEnvironment/CloudModule.cs
@@ -9,7 +9,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Universe Project nor the
+ *     * Neither the name of the Virtual Universe Project nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
@@ -26,12 +26,12 @@
  */
 
 using System;
+using Nini.Config;
 using Universe.Framework.Modules;
 using Universe.Framework.PresenceInfo;
 using Universe.Framework.SceneInfo;
-using Nini.Config;
 
-namespace Universe.Modules.Cloud
+namespace Universe.Modules.GlobalEnvironment
 {
     public class CloudModule : ICloudModule
     {
--- Universe/Modules/World/GlobalEnvironment/Plugins/SimpleRandomWind.cs
+++ Universe/Modules/World/GlobalEnvironment/Plugins/SimpleRandomWind.cs
@@ -9,7 +9,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Universe Project nor the
+ *     * Neither the name of the Virtual Universe Project nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
@@ -33,7 +33,7 @@ using Universe.Framework.Modules;
 using Universe.Framework.SceneInfo;
 
 
-namespace Universe.Modules.Wind.Plugins
+namespace Universe.Modules.GlobalEnvironment.Plugins
 {
     public class SimpleRandomWind : IWindModelPlugin
     {
