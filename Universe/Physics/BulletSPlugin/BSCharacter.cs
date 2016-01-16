afb537df83f84f10b3e552a66d133dad966de4c4
EmperorStarfinder <emperor@secondgalaxy.com>
2016-01-16 18:02:57 -0500

    Licensing
    - Correcting the urls in the license
    
    Signed-off-by: EmperorStarfinder <emperor@secondgalaxy.com>

--- Universe/Physics/BulletSPlugin/BSCharacter.cs
+++ Universe/Physics/BulletSPlugin/BSCharacter.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) Contributors, http://virtual-planets.org/, http://whitecore-sim.org/, http://opensimulator.org, http://universe-sim.org
+ * Copyright (c) Contributors, http://virtual-planets.org/, http://whitecore-sim.org/, http://aurora-sim.org/, http://opensimulator.org,
  * See CONTRIBUTORS.TXT for a full list of copyright holders.
  *
  * Redistribution and use in source and binary forms, with or without
