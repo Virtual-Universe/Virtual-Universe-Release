1a76d2c19b1c7d7635b9fe3b6d0719313cf288eb
EmperorStarfinder <emperor@secondgalaxy.com>
2016-02-05 15:24:31 -0500

    AssemblyInfo updates
    Signed-off-by: EmperorStarfinder <emperor@secondgalaxy.com>

--- Universe/Physics/OpenDynamicsEngine/Properties/AssemblyInfo.cs
+++ Universe/Physics/OpenDynamicsEngine/Properties/AssemblyInfo.cs
@@ -1,5 +1,5 @@
-/*
- * Copyright (c) Contributors, http://virtual-planets.org/, http://aurora-sim.org
+﻿/*
+ * Copyright (c) Contributors, http://virtual-planets.org/, http://whitecore-sim.org/, http://aurora-sim.org, http://opensimulator.org/
  * See CONTRIBUTORS.TXT for a full list of copyright holders.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -9,7 +9,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Virtual Universe Project nor the
+ *     * Neither the name of the Virtual-Universe Project nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
@@ -25,33 +25,49 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+
+using System;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Resources;
 
-// Information about this assembly is defined by the following
-// attributes.
-//
-// change them to the information which is associated with the assembly
-// you compile.
-
-[assembly: AssemblyTitle("UniverseOdePlugin")]
+/// <summary>
+/// General information about an assembly is controlled through the following
+/// set of attributes.  Change these attribute values to modify the information
+/// associated with an assembly.
+/// </summary>
+[assembly: AssemblyTitle("Universe.Physics.OpenDynamicEngine")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("http://Universe-sim.org")]
-[assembly: AssemblyCopyright("Copyright © 2014-2016")]
+[assembly: AssemblyCompany("Virtual Universe Development Team")]
+[assembly: AssemblyProduct("Universe.Physics.OpenDynamicEngine")]
+[assembly: AssemblyCopyright("Copyright © 2015-2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// This sets the default COM visibility of types in the assembly to invisible.
-// If you need to expose a type to COM, use [ComVisible(true)] on that type.
-
+/// <summary>
+/// Setting ComVisible to false makes the types in this assembly not visible 
+/// to COM components.  If you need to access a type in this assembly from 
+/// COM, set the ComVisible attribute to true on that type.
+/// </summary>
 [assembly: ComVisible(false)]
 
-// The assembly version has following format :
-//
-// Major.Minor.Build.Revision
-//
-// You can specify all values by your own or you can build default build and revision
-// numbers with the '*' character (the default):
+/// <summary>
+/// The following GUID is for the ID of the typelib if this project is exposed to COM
+/// </summary>
+[assembly: Guid("269c99ff-0740-4886-8593-7527903090bf")]
 
-[assembly: AssemblyVersion("0.9.4.*")]
+/// <summary>
+/// Version information for an assembly consists of the following four values:
+///
+///      Major Version
+///      Minor Version 
+///      Build Number
+///      Revision
+///
+/// You can specify all the values or you can default the Build and Revision Numbers 
+/// by using the '*' as shown below:
+/// [assembly: AssemblyVersion("1.0.*")]
+/// </summary>
+[assembly: AssemblyVersion("1.0.1.*")]
+[assembly: NeutralResourcesLanguageAttribute("en")]
