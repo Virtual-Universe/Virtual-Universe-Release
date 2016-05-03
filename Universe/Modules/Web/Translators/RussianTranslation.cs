16328ce58f6e285e5f33acde0db6cf73dd8e0f3b
EmperorStarfinder <emperor@secondgalaxy.com>
2016-05-03 07:17:06 -0400

    Fix wrong references in translator
    Signed-Off-By: EmperorStarfinder<emperor@secondgalaxy.com>

--- Universe/Modules/Web/Translators/RussianTranslation.cs
+++ Universe/Modules/Web/Translators/RussianTranslation.cs
@@ -9,7 +9,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Vision-Sim Project nor the
+ *     * Neither the name of the Virtual Universe Project nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
@@ -25,7 +25,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-namespace Vision.Modules.Web.Translators
+namespace Universe.Modules.Web.Translators
 {
 	public class RussianTranslation : ITranslator
 	{
