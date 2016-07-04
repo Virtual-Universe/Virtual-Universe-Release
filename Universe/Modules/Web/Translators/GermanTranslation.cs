44a54391220db2602a1d3e53ae877e287688e651
IrishwolfStarfinder <irishwolf@secondgalaxy.com>
2016-07-04 11:22:56 -0400

    Quick cleanup
    Signed-off-by: IrishwolfStarfinder <irishwolf@secondgalaxy.com>

--- Universe/Modules/Web/Translators/GermanTranslation.cs
+++ Universe/Modules/Web/Translators/GermanTranslation.cs
@@ -25,7 +25,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-namespace WhiteCore.Modules.Web.Translators
+namespace Universe.Modules.Web.Translators
 {
     public class GermanTranslation : ITranslator
     {
--- Universe/Services/API/WorldAPI/Region.cs
+++ Universe/Services/API/WorldAPI/Region.cs
@@ -24,7 +24,9 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
- 
+
+using Nini.Config;
+using Universe.Framework.Modules;
 using Universe.Framework.Services;
 
 namespace Universe.Services.API.WorldAPI
