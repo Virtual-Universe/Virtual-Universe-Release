6da9f99b8bf70f9941511bf9963dfba0a42d6f2f
EmperorStarfinder <emperor@secondgalaxy.com>
2016-08-05 16:00:18 -0400

    Fix the license in EmptyClass.cs so it provides the correct information
    Signed-Off-By: EmperorStarfinder<emperor@secondgalaxy.com>

--- BuildingTools/EmptyClass.cs
+++ BuildingTools/EmptyClass.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Copyright (c) Contributors, http://secondgalaxy.com/
+ * Copyright (c) Contributors, http://virtual-planets.org/
  * See CONTRIBUTORS.TXT for a full list of copyright holders.
  * For an explanation of the license of each contributor and the content it 
  * covers please see the Licenses directory.
@@ -11,7 +11,7 @@
  *     * Redistributions in binary form must reproduce the above copyright
  *       notice, this list of conditions and the following disclaimer in the
  *       documentation and/or other materials provided with the distribution.
- *     * Neither the name of the Second Galaxy Project nor the
+ *     * Neither the name of the Virtual Universe Project nor the
  *       names of its contributors may be used to endorse or promote products
  *       derived from this software without specific prior written permission.
  *
@@ -25,6 +25,7 @@
  * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 using System;
 
