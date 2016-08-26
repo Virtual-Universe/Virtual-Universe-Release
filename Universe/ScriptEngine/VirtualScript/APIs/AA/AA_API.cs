7e5744c7ac278c6ad36382094ef00c6c2ca637eb
BritanyannStarfinder <britanyann@secondgalaxy.com>
2016-08-26 16:43:09 -0400

    A quick cleanup
    Signed-Off-By: BritanyannStarfinder<britanyann@secondgalaxy.com>

--- Universe/ScriptEngine/VirtualScript/APIs/AA/AA_API.cs
+++ Universe/ScriptEngine/VirtualScript/APIs/AA/AA_API.cs
@@ -714,9 +714,10 @@ namespace Universe.ScriptEngine.VirtualScript.APIs
 
             if (lease.CurrentState == LeaseState.Initial) {
                 lease.InitialLeaseTime = TimeSpan.FromMinutes (0);
-                // lease.RenewOnCallTime = TimeSpan.FromSeconds(10.0);
-                // lease.SponsorshipTimeout = TimeSpan.FromMinutes(1.0);
+                //lease.RenewOnCallTime = TimeSpan.FromSeconds(10.0);
+                //lease.SponsorshipTimeout = TimeSpan.FromMinutes(1.0);
             }
+
             return lease;
         }
 
